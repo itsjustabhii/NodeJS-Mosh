2dea811c150d6a34e3b877fdb69251fabe4b5b9c
Abhishek Harish <itsjustabhii@gmail.com>
2024-11-01 17:17:14 +0530

    added an endpoint to display the array selected

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ import express from 'express';
 //calling the object express
 const app = express()
 
+const courses = [
+    {id:1, name: 'course1'},
+    {id:2, name: 'course2'},
+    {id:3, name: 'course3'}
+]
+
 //GET Method
 //Take two arguments, path and callback function(aka route handler)
 app.get('/', (req,res)=>{
@@ -15,6 +21,13 @@ app.get('/api/courses', (req,res)=>{
 //to read the value of parameter
 app.get('/api/courses/:id', (req,res)=>{
     res.send(req.params.id)
+})
+
+//creating and API to search the array
+app.get('/api/courses/:id',(req,res)=>{
+    const course = courses.find(c => c.id === parseInt(req.params.id))
+    if (!course) res.status(404).send('The course with given ID was not found!')
+        res.send(course)
 })
 
 //Creating dynamic porting address
