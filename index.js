b139cecb7bf54028eac90a0a0348d18d7dc2d59e
Abhishek Harish <itsjustabhii@gmail.com>
2024-11-01 16:54:05 +0530

    to read parameter values

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ app.get('/api/courses', (req,res)=>{
     res.send([1,2,3])
 })
 
+//to read the value of parameter
+app.get('/api/courses/:id', (req,res)=>{
+    res.send(req.params.id)
+})
+
 //Creating dynamic porting address
 const port = process.env.PORT || 3000
 app.listen(port, ()=> console.log(`Listening to ${port}.....`))
