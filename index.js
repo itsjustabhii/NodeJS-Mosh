42dda6efea01c09907e9f8fee7ccfb324a0ffccc
Abhishek Harish <itsjustabhii@gmail.com>
2024-11-01 16:36:43 +0530

    added another GET endpoint

--- index.js
+++ index.js
@@ -8,4 +8,8 @@ app.get('/', (req,res)=>{
     res.send("Hello World!")
 })
 
+app.get('/api/courses', (req,res)=>{
+    res.send([1,2,3])
+})
+
 app.listen(3000, ()=> console.log('Listening on Port 3000...'))
