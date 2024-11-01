cdf073c585544888c06d5a72d9e24c03cc9c7247
Abhishek Harish <itsjustabhii@gmail.com>
2024-11-01 15:58:37 +0530

    Added http server module

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
-function sayHello(name){
-    console.log('Hello ' + name)
-}
+// function sayHello(name){
+//     console.log('Hello ' + name)
+// }
 
-sayHello("Abhishek")
+// sayHello("Abhishek")
+
+import http from 'http';
+
+const server = http.createServer((req,res) =>{
+    if(req.url ==='/'){
+        res.write('Hello world')
+        res.end()
+    }
+
+    if(req.url === '/api/courses'){
+        res.write(JSON.stringify([1,2,3]))
+        res.end()
+    }
+})
+
+server.listen(3000)
+
+console.log("Listening to server 3000...")
